1db48de2f27df9b51cc990de5c79531a90cea4e0
203.autom <203.autom@agrom.mousquetaires.com>
2025-01-22 08:59:32 +0100

    Parametrage de l'adresse IP et du mask

--- CreationDeProjet/CreationDeProjet.cs
+++ CreationDeProjet/CreationDeProjet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -9,6 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Siemens.Engineering;
+using Siemens.Engineering.HW;
+using Siemens.Engineering.HW.Features;
 
 namespace CreationDeProjet
 {
@@ -16,12 +19,13 @@ namespace CreationDeProjet
     {
         public CreationDeProjet()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
 
-        private TiaPortal tiaPortal;
-        private Project tiaProject;
-       
+        private TiaPortal oTiaPortal;
+        private Project oTiaProject;
+        private TIAGenerator oTIAGenerator = new TIAGenerator();
+
         //Ajout du nom du projet sans espace
         private void txtInputName_TextChanged(object sender, EventArgs e)
         { 
@@ -53,29 +57,25 @@ namespace CreationDeProjet
         //Génération du projet
         private void bpGeneration_Click(object sender, EventArgs e)
         {
-            try
+            string sError = string.Empty;
+
+            while (true)
             {
-                //Vérifiez si TIA Portal est déjà en cours d'exécution
-                tiaPortal = new TiaPortal(TiaPortalMode.WithUserInterface);
 
-                //Configuration du nom et du chemin du projet
-                string projectName = txtInputName.Text;
-                string projectPath = txtInputPath.Text;
-
-                DirectoryInfo directoryInfo = new DirectoryInfo(projectPath);
-
-                if (!directoryInfo.Exists)
+                if (!oTIAGenerator.ProjectGenerator(ref sError, ref oTiaPortal, ref oTiaProject, txtInputName.Text, txtInputPath.Text)) 
                 {
-                    directoryInfo.Create();
+                    Console.WriteLine(sError);
+                    break;
+                }
+                if (!oTIAGenerator.DeviceGenerator(ref sError, ref oTiaProject))
+                {
+                    Console.WriteLine(sError);
+                    break;
                 }
 
-                //Créez le projet
-                tiaProject = tiaPortal.Projects.Create(directoryInfo, projectName);
+                break;
             }
-            catch (Exception ex)
-            {
-                ;
-            }
+            
         }
 
     }
---
+++ CreationDeProjet/TIAGenerator.cs
@@ -0,0 +1,130 @@
+﻿using Siemens.Engineering.HW;
+using Siemens.Engineering;
+using Siemens.Engineering.HW.Features;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Siemens.Engineering.Online;
+using Siemens.Engineering.Connection;
+
+namespace CreationDeProjet
+{
+    internal class TIAGenerator
+    {
+        public bool ProjectGenerator(ref string sError, ref TiaPortal oTiaPortal, ref Project oTiaProject, string sProjecName, string sProjectPath)
+        {
+            try
+            {
+                //Vérifiez si TIA Portal est déjà en cours d'exécution
+                oTiaPortal = new TiaPortal(TiaPortalMode.WithUserInterface);
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(sProjectPath);
+
+                if (!directoryInfo.Exists)
+                {
+                    directoryInfo.Create();
+                }
+
+                //Créez le projet
+                oTiaProject = oTiaPortal.Projects.Create(directoryInfo, sProjecName);
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool DeviceGenerator(ref string sError, ref Project oTiaProject)
+        {
+            try
+            {
+                if (oTiaProject == null)
+                {
+                    sError = "Aucun projet ouvert. Veuillez créer ou ouvrir un projet d'abord.";
+                    return false;
+                }
+                DeviceComposition devices = oTiaProject.Devices;
+                Device deviceWithItem = devices.CreateWithItem(
+                    "OrderNumber:6ES7 511-1AL03-0AB0/V3.1", // Numéro de commande pour un S7-1510
+                    "PLC_1",                                // Nom de l'élément d'appareil
+                    "MyPLC"                                 // Nom de l'appareil
+                );
+
+                // Vérifier si l'appareil a été créé
+                if (deviceWithItem == null)
+                {
+                    sError = "Échec de la création de l'appareil.";
+                    return false;
+                }
+
+                SetNodeAddressAndMask(deviceWithItem, "192.168.07.99", "255.255.0.0");
+
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return false;
+            }
+            return true;
+        }
+
+        public static void SetNodeAddressAndMask(Device device, string address, string subnetMask)
+        {
+            //set PLC IP address and subnet mask
+            Console.WriteLine("Changing " + device.Name + " IP address to " + address);
+            if (device != null)
+            {
+                NetworkInterface itf = null;
+                int idx1 = 0, idx2 = 0;
+                bool foundIt = false;
+
+                for (idx1 = 0; idx1 < device.DeviceItems.Count; idx1++)
+                {
+                    DeviceItem di1 = device.DeviceItems.ElementAt(idx1);
+                    for (idx2 = 0; idx2 < di1.DeviceItems.Count; idx2++)
+                    {
+                        DeviceItem di2 = di1.DeviceItems.ElementAt(idx2);
+                        itf = ((IEngineeringServiceProvider)di2).GetService<NetworkInterface>();
+                        if (itf != null)
+                        {
+                            foundIt = true;
+                            break;
+                        }
+                    }
+                    if (foundIt)
+                    {
+                        Console.WriteLine("Found network interface at " + idx1 + ", " + idx2);
+                        break;
+                    }
+
+                }
+                if (foundIt)
+                {
+                    //change the IP address
+                    Node node = itf.Nodes.ElementAt(0);
+                    if (node != null)
+                    {
+                        IEngineeringObject nodeObject = (IEngineeringObject)node;
+                        nodeObject.SetAttribute("Address", address);
+                        
+                    }
+                    else
+                        Console.WriteLine("Unable to locate node");
+                }
+                else
+                    Console.WriteLine("Unable to locate profinet interface");
+
+            }
+            else
+                Console.WriteLine("Device is null");
+
+
+        }
+    }
+}
